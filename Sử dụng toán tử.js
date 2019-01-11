622047d6dda9c13119b9887cdab77280e47ba9b3
Bdch90 <46187905+Bdch90@users.noreply.github.com>
2019-01-11 11:01:22 +0700

    Delete Sử dụng toán tử.js

--- Sử dụng toán tử.js
+++
@@ -1,9 +0,0 @@
-var width;
-var heigth;
-
-var width = parseInt(prompt("enter the width"));
-var heigth = parseInt(promt("inter the heigth"));
-
-var area = width * heigth;
-document.write("area = " + area);
-
